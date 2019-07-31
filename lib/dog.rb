2426687828dfc1dab0aea182433ab62acebda578
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-31 20:12:35 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -82,7 +82,7 @@ attr_accessor :id, :name, :breed
     UPDATE dogs SET name = ?, breed = ? WHERE id = ?
     SQL
     
-    DB[:conn].execute(sql, self.name, self.breed)
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
   end 
     
   def save
