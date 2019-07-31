c022d0d57cc33e03b823d153d8917feae30c6ac0
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-31 20:00:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -69,7 +69,7 @@ attr_accessor :id, :name, :breed
   end
   
   def self.create(name, breed)
-    dog = Dog.new(name, breed)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     dog
   end
