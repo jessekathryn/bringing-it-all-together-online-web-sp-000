fc7f0b7580c0611e88d8f22b3c4231ec1350ec43
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-07-31 20:09:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,6 +40,7 @@ attr_accessor :id, :name, :breed
   def self.find_by_name(name)
     sql = <<-SQL
      SELECT * FROM dogs WHERE name = ?
+     LIMIT 1
      SQL
      
      DB[:conn].execute(self.name)
